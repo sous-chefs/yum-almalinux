fc1d9c98b87752232caf6029e38c028ab6e7e8c1
Ethan Hampton <hamptone@osuosl.org>
2023-01-17 11:17:22 -0800

    Update copyright
    Signed-off-by: Ethan Hampton <hamptone@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'yum-almalinux'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures yum-almalinux'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description 'Installs/Configures AlmaLinux yum repositories'
 version '0.1.0'
 chef_version '>= 16.0'
 
