f42d96f04eb65b65a6ec0d6ab74cd7fb621421db
Ethan Hampton <hamptone@osuosl.org>
2023-01-19 10:43:16 -0800

    Add rest of AlmaLinux repos
    Signed-off-by: Ethan Hampton <hamptone@osuosl.org>

---
+++ resources/alma_saphana.rb
@@ -0,0 +1,47 @@
+provides :yum_alma_saphana
+unified_mode true
+
+use '_partials/_common'
+
+repo_name = 'SAPHANA'
+
+property :baseurl, String, default: lazy { alma_repo_baseurl(repo_name) }
+property :mirrorlist, String, default: lazy { alma_repo_mirrorlist(repo_name) }
+property :description, String, default: lazy { alma_repo_description(repo_name) }
+
+property :debug_baseurl, String, default: lazy { alma_repo_baseurl(repo_name, true) }
+property :debug_mirrorlist, String, default: lazy { alma_repo_mirrorlist(repo_name, true) }
+property :debug_description, String, default: lazy { alma_repo_description(repo_name, true) }
+
+action_class do
+  include YumAlmaChef::Cookbook::Helpers
+end
+
+action :create do
+  return unless platform_family?('rhel')
+
+  # remove existing repo configs
+  ::Dir['/etc/yum.repos.d/almalinux*'].each do |f|
+    file f do
+      action :delete
+    end
+  end
+
+  yum_repository repo_name.downcase do
+    baseurl new_resource.baseurl
+    mirrorlist new_resource.mirrorlist
+    description new_resource.description
+    enabled new_resource.enabled
+    gpgcheck new_resource.gpgcheck
+    gpgkey new_resource.gpgkey
+  end
+
+  yum_repository "#{repo_name.downcase}-debuginfo" do
+    baseurl new_resource.debug_baseurl
+    mirrorlist new_resource.debug_mirrorlist
+    description new_resource.debug_description
+    enabled new_resource.debug_enabled
+    gpgcheck new_resource.gpgcheck
+    gpgkey new_resource.gpgkey
+  end
+end
